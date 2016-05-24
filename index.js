9ba3a19f6fbe729256b3fad01a7a58e653a4dbd7
Carl Larson <carl@powster.com>
2016-05-24 12:16:37 +0100

    convert to sync

--- index.js
+++ index.js
@@ -45,30 +45,32 @@ function AtImport(options) {
       throw new Error("plugins option must be an array")
     }
 
-    return parseStyles(
-      result,
-      styles,
-      options,
-      state,
-      []
-    ).then(function(bundle) {
+    var bundle = parseStyles(result,styles,options,state,[]);
 
-      applyRaws(bundle)
-      applyMedia(bundle)
-      applyStyles(bundle, styles)
+    applyRaws(bundle)
+    applyMedia(bundle)
+    applyStyles(bundle, styles)
 
-      if (
-        typeof options.addDependencyTo === "object" &&
-        typeof options.addDependencyTo.addDependency === "function"
-      ) {
-        Object.keys(state.importedFiles)
-        .forEach(options.addDependencyTo.addDependency)
-      }
+    if (
+      typeof options.addDependencyTo === "object" &&
+      typeof options.addDependencyTo.addDependency === "function"
+    ) {
+      Object.keys(state.importedFiles)
+      .forEach(options.addDependencyTo.addDependency)
+    }
 
-      if (typeof options.onImport === "function") {
-        options.onImport(Object.keys(state.importedFiles))
-      }
-    })
+    if (typeof options.onImport === "function") {
+      options.onImport(Object.keys(state.importedFiles))
+    }
+
+    // return parseStyles(
+    //   result,
+    //   styles,
+    //   options,
+    //   state,
+    //   []
+    // ).then(function(bundle) {
+    // })
   }
 }
 
@@ -163,51 +165,92 @@ function parseStyles(
 ) {
   var statements = parseStatements(result, styles)
 
-  return Promise.all(statements.map(function(stmt) {
+  statements.forEach(function(stmt) {
     stmt.media = joinMedia(media, stmt.media)
 
     // skip protocol base uri (protocol://url) or protocol-relative
     if (stmt.type !== "import" || /^(?:[a-z]+:)?\/\//i.test(stmt.uri)) {
       return
     }
-    return resolveImportId(
-      result,
-      stmt,
-      options,
-      state
-    )
-  })).then(function() {
-    var imports = []
-    var bundle = []
-
-    // squash statements and their children
-    statements.forEach(function(stmt) {
-      if (stmt.type === "import") {
-        if (stmt.children) {
-          stmt.children.forEach(function(child, index) {
-            if (child.type === "import") {
-              imports.push(child)
-            }
-            else {
-              bundle.push(child)
-            }
-            // For better output
-            if (index === 0) {
-              child.parent = stmt
-            }
-          })
-        }
-        else {
-          imports.push(stmt)
-        }
-      }
-      else if (stmt.type === "media" || stmt.type === "nodes") {
-        bundle.push(stmt)
-      }
-    })
-
-    return imports.concat(bundle)
+    resolveImportId(result,stmt,options,state)
   })
+
+  var imports = []
+  var bundle = []
+
+  // squash statements and their children
+  statements.forEach(function(stmt) {
+    if (stmt.type === "import") {
+      if (stmt.children) {
+        stmt.children.forEach(function(child, index) {
+          if (child.type === "import") {
+            imports.push(child)
+          }
+          else {
+            bundle.push(child)
+          }
+          // For better output
+          if (index === 0) {
+            child.parent = stmt
+          }
+        })
+      }
+      else {
+        imports.push(stmt)
+      }
+    }
+    else if (stmt.type === "media" || stmt.type === "nodes") {
+      bundle.push(stmt)
+    }
+  })
+
+  return imports.concat(bundle)
+
+  // return SyncPromise.all(statements.map(function(stmt) {
+  //   stmt.media = joinMedia(media, stmt.media)
+  //
+  //   // skip protocol base uri (protocol://url) or protocol-relative
+  //   if (stmt.type !== "import" || /^(?:[a-z]+:)?\/\//i.test(stmt.uri)) {
+  //     return
+  //   }
+  //   return resolveImportId(
+  //     result,
+  //     stmt,
+  //     options,
+  //     state
+  //   )
+  // })).then(function() {
+  //   var imports = []
+  //   var bundle = []
+  //
+  //   // squash statements and their children
+  //   statements.forEach(function(stmt) {
+  //     if (stmt.type === "import") {
+  //       if (stmt.children) {
+  //         stmt.children.forEach(function(child, index) {
+  //           if (child.type === "import") {
+  //             imports.push(child)
+  //           }
+  //           else {
+  //             bundle.push(child)
+  //           }
+  //           // For better output
+  //           if (index === 0) {
+  //             child.parent = stmt
+  //           }
+  //         })
+  //       }
+  //       else {
+  //         imports.push(stmt)
+  //       }
+  //     }
+  //     else if (stmt.type === "media" || stmt.type === "nodes") {
+  //       bundle.push(stmt)
+  //     }
+  //   })
+  //
+  //   return imports.concat(bundle)
+  // })
 }
 
 function resolveImportId(
@@ -221,33 +264,27 @@ function resolveImportId(
     ? path.dirname(atRule.source.input.file)
     : options.root
 
-  return Promise.resolve(options.resolve(stmt.uri, base, options))
-  .then(function(resolved) {
-    if (!Array.isArray(resolved)) {
-      resolved = [ resolved ]
+  var resolved = options.resolve(stmt.uri, base, options)
+  if (!Array.isArray(resolved)) {
+    resolved = [ resolved ]
+  }
+
+  var content = resolved.map(function(file) {
+    return loadImportContent(
+      result,
+      stmt,
+      file,
+      options,
+      state
+    )
+  });
+
+  stmt.children = content.reduce(function(result, statements) {
+    if (statements) {
+      result = result.concat(statements)
     }
-    return Promise.all(resolved.map(function(file) {
-      return loadImportContent(
-        result,
-        stmt,
-        file,
-        options,
-        state
-      )
-    }))
-  })
-  .then(function(result) {
-    // Merge loaded statements
-    stmt.children = result.reduce(function(result, statements) {
-      if (statements) {
-        result = result.concat(statements)
-      }
-      return result
-    }, [])
-  })
-  .catch(function(err) {
-    result.warn(err.message, { node: atRule })
-  })
+    return result
+  }, [])
 }
 
 function loadImportContent(
@@ -275,62 +312,51 @@ function loadImportContent(
     state.importedFiles[filename][media] = true
   }
 
-  return Promise.resolve(options.load(filename, options))
-  .then(function(content) {
-    if (typeof options.transform !== "function") {
-      return content
-    }
-    return Promise.resolve(options.transform(content, filename, options))
-    .then(function(transformed) {
-      return typeof transformed === "string" ? transformed : content
+  var content = options.load(filename, options);
+  // if (typeof options.transform !== "function") {
+  //   return content
+  // }
+  if (typeof options.transform === "function") {
+    var transformed = options.transform(content, filename, options)
+    content = typeof transformed === "string" ? transformed : content
+  }
+
+  if (content.trim() === "") {
+    result.warn(filename + " is empty", { node: atRule })
+    return
+  }
+
+  // skip previous imported files not containing @import rules
+  if (state.hashFiles[content] && state.hashFiles[content][media]) {
+    return
+  }
+
+  var result = postcss(options.plugins).process(content, {from: filename,syntax: result.opts.syntax,parser: result.opts.parser,}).css
+
+  var styles = result.root
+  result.messages = result.messages.concat(result.messages)
+
+  if (options.skipDuplicates) {
+    var hasImport = styles.some(function(child) {
+      return child.type === "atrule" && child.name === "import"
     })
-  })
-  .then(function(content) {
-    if (content.trim() === "") {
-      result.warn(filename + " is empty", { node: atRule })
-      return
-    }
-
-    // skip previous imported files not containing @import rules
-    if (
-      state.hashFiles[content] &&
-      state.hashFiles[content][media]
-    ) {
-      return
-    }
-
-    return postcss(options.plugins).process(content, {
-      from: filename,
-      syntax: result.opts.syntax,
-      parser: result.opts.parser,
-    })
-    .then(function(importedResult) {
-      var styles = importedResult.root
-      result.messages = result.messages.concat(importedResult.messages)
-
-      if (options.skipDuplicates) {
-        var hasImport = styles.some(function(child) {
-          return child.type === "atrule" && child.name === "import"
-        })
-        if (!hasImport) {
-          // save hash files to skip them next time
-          if (!state.hashFiles[content]) {
-            state.hashFiles[content] = {}
-          }
-          state.hashFiles[content][media] = true
-        }
+    if (!hasImport) {
+      // save hash files to skip them next time
+      if (!state.hashFiles[content]) {
+        state.hashFiles[content] = {}
       }
+      state.hashFiles[content][media] = true
+    }
+  }
 
-      // recursion: import @import from imported file
-      return parseStyles(
-        result,
-        styles,
-        options,
-        state,
-        media
-      )
-    })
-  })
+  // recursion: import @import from imported file
+  return parseStyles(
+    result,
+    styles,
+    options,
+    state,
+    media
+  )
 }
 
 module.exports = postcss.plugin(
